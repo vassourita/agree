dfe1caf4e83b992ddbc276a3261794cb452b1975
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-08-18 09:59:51 -0300

    Lint

--- .jestrc.js
+++ .jestrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
-  projects: ['<rootDir>/libs/**/jest.config.js', '<rootDir>/apps/**/jest.config.js'],
+  projects: [
+    '<rootDir>/libs/**/jest.config.js',
+    '<rootDir>/apps/**/jest.config.js'
+  ],
   testEnvironment: 'node',
   testMatch: ['*.spec.ts', '*.spec.tsx']
 }
