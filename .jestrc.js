8cb7b881e032c2cbf0acb99bc5bf363c87002ab5
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-08-18 09:05:22 -0300

    Jest workspace setup

---
+++ .jestrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  projects: ['<rootDir>/libs/**/jest.config.js', '<rootDir>/apps/**/jest.config.js'],
+  testEnvironment: 'node',
+  testMatch: ['*.spec.ts', '*.spec.tsx']
+}
