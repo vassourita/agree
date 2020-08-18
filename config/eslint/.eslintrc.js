c0f5589e0859424a811994ac3edc84144716e1ce
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-08-18 08:58:43 -0300

    Eslint setup

---
+++ config/eslint/.eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+    'prettier/react'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'import-helpers'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'space-before-function-paren': 'off',
+    'no-useless-constructor': 'off',
+    'no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '_'
+      }
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          ['/^next/', '/^react/', '/^@nestjs/'],
+          'module',
+          ['parent', 'sibling', 'index']
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true
+        }
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    },
+    react: {
+      version: 'detect'
+    }
+  }
+}
