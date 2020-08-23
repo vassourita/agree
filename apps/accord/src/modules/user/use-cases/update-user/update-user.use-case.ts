e3e2969783048216964d2b5e26b11fc38e9e9218
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-08-23 14:43:28 -0300

    Updated avatar upload use-case to delete old avatar file

--- apps/accord/src/modules/user/use-cases/update-user/update-user.use-case.ts
+++ apps/accord/src/modules/user/use-cases/update-user/update-user.use-case.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { InjectRepository } from '@nestjs/typeorm'
 
 import { IUseCase } from '@shared/protocols/use-case'
+import { unlink } from 'fs'
+import { join } from 'path'
 import { Repository } from 'typeorm'
 
 import { UserEntity } from '../../entities/user.entity'
@@ -11,7 +14,8 @@ import { UpdateUserDTO } from './update-user.dto'
 export class UpdateUserUseCase implements IUseCase<UpdateUserDTO, UserEntity> {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
+    private readonly userRepository: Repository<UserEntity>,
+    private readonly config: ConfigService
   ) {}
 
   async execute(data: UpdateUserDTO) {
@@ -24,6 +28,8 @@ export class UpdateUserUseCase implements IUseCase<UpdateUserDTO, UserEntity> {
       user.status = data.status
     }
     if (data.avatar) {
+      const pathToOldAvatar = join(this.config.get('upload.dir'), user.avatar)
+      unlink(pathToOldAvatar, () => 0)
       user.avatar = data.avatar
     }
 
