5aa2a2801ba5b507ceb2e08e9b318bd50d970aee
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-08-27 19:43:17 -0300

    Added jest path module name mapper

--- apps/accord/jest.config.js
+++ apps/accord/jest.config.js
@@ -1,15 +1,19 @@
+/* eslint-disable import-helpers/order-imports */
 const { name } = require('./package.json')
+const tsconfig = require('./tsconfig.json')
+
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig)
 
 module.exports = {
   displayName: name,
   name,
   preset: 'ts-jest',
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.spec.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest'
   },
   coverageDirectory: '../coverage',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  moduleNameMapper
 }
