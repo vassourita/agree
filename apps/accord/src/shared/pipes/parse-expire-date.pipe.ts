225d932a624ec0394f419252bf778ce5817fa7db
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-08-29 15:59:14 -0300

    Added optional no-expire invites

--- apps/accord/src/shared/pipes/parse-expire-date.pipe.ts
+++ apps/accord/src/shared/pipes/parse-expire-date.pipe.ts
@@ -4,6 +4,10 @@ import ms from 'ms'
 
 export class ParseExpireDatePipe implements PipeTransform {
   transform(value: any, _metadata: ArgumentMetadata) {
+    if (!value) {
+      return undefined
+    }
+
     if (typeof value !== 'string') {
       throw new BadRequestException('Invalid expire date')
     }
