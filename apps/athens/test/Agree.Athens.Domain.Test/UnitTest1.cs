a8e47684cc8933a94da14f3e52fef775e83d44ff
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2021-02-15 19:16:11 -0300

    Created first test project

---
+++ apps/athens/test/Agree.Athens.Domain.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Agree.Athens.Domain.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
