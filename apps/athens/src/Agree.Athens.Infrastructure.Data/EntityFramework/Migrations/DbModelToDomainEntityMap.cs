14eca6d0562271c72b0f9327ab0cba0419bed3cc
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2021-02-23 13:01:15 -0300

    Added model do entity map and automapper

---
+++ apps/athens/src/Agree.Athens.Infrastructure.Data/EntityFramework/Migrations/DbModelToDomainEntityMap.cs
@@ -0,0 +1,29 @@
+using Agree.Athens.Domain.Aggregates.Account;
+using Agree.Athens.Domain.Aggregates.Messages;
+using Agree.Athens.Domain.Aggregates.Servers;
+using Agree.Athens.Infrastructure.Data.EntityFramework.DataModels;
+using AutoMapper;
+
+namespace Agree.Athens.Infrastructure.Data.EntityFramework.Migrations
+{
+    public class DbModelToDomainEntityMap : Profile
+    {
+        public DbModelToDomainEntityMap()
+        {
+            CreateMap<UserDbModel, UserAccount>()
+                .ReverseMap();
+
+            CreateMap<UserDbModel, Author>()
+                .ForMember(
+                    author => author.Active,
+                    config => config.MapFrom(dbModel => dbModel.DeletedAt != null)
+                ).ReverseMap();
+
+            CreateMap<UserDbModel, User>()
+                .ForMember(
+                    user => user.Active,
+                    config => config.MapFrom(dbModel => dbModel.DeletedAt != null)
+                ).ReverseMap();
+        }
+    }
+}
