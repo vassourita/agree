85aadc26612d10988097591a8218189182971865
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2021-03-01 11:54:33 -0300

    Added typescript

---
+++ apps/agree-landing/src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import {Html,Head,Main,NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+  public render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@400;500;700&display=swap" rel="stylesheet"/>
+          <title>Agree</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ apps/agree-landing/src/pages/index.tsx
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <nav></nav>
+        <div></div>
+        <h1></h1>
+      </header>
+      <main>
+        <section>
+          <h3></h3>
+          <p></p>
+        </section>
+        <section>
+          <h3></h3>
+          <p></p>
+        </section>
+        <section>
+          <h3></h3>
+          <p></p>
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  )
+}
