aa155a1288a8058461f78e3ac4900277b746dae4
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2021-03-01 23:54:52 -0300

    Updated landing texts

--- apps/agree-landing/src/pages/index.tsx
+++ apps/agree-landing/src/pages/index.tsx
@@ -1,13 +1,11 @@
-import { motion, useTransform, useViewportScroll } from 'framer-motion'
-import { FiChevronDown, FiDollarSign, FiDownload, FiGithub, FiGlobe, FiHelpCircle, FiSmartphone } from 'react-icons/fi'
+import { motion } from 'framer-motion'
+import { FiChevronDown, FiCreditCard, FiDollarSign, FiDownload, FiGithub, FiGlobe, FiHelpCircle, FiLogIn, FiSmartphone } from 'react-icons/fi'
+import { AiOutlineQrcode } from 'react-icons/ai'
 import { useEffect } from 'react'
 import { useMedia } from 'react-use'
 
 export default function Home () {
   const isSmallScreen = useMedia('(max-width: 768px)', false)
-  const { scrollYProgress } = useViewportScroll()
-  const opacity = useTransform(scrollYProgress, [0, 1], [0.2, 1])
-  console.log(opacity)
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -37,11 +35,8 @@ export default function Home () {
   return (
     <motion.div className="h-screen w-full">
       <motion.header
-        initial={{ opacity: 0 }}
-        animate={{ opacity: opacity.get() }}
-        transition={{
-          type: 'just'
-        }}
+        initial={{ opacity: 0, x: -200 }}
+        animate={{ opacity: 1, x: 0, transition: { ease: 'easeOut' } }}
         className="bg-primary h-full"
       >
         <div className="container h-full m-auto flex flex-col justify-between">
@@ -70,7 +65,7 @@ export default function Home () {
                 <FiGithub className="cursor-pointer transform transition-all hover:scale-125 h-6 md:h-8 w-auto" />
               </motion.a>
               <motion.a
-                variants={loadingItemsVariants}MotionValue
+                variants={loadingItemsVariants}
                 href="#donate"
               >
                 <FiDollarSign className="cursor-pointer transform transition-all hover:scale-125 h-6 md:h-8 w-auto" />
@@ -91,9 +86,9 @@ export default function Home () {
               <span className="text-text-dark font-bold mr-4 md:mr-0">Em breve</span>
               <button
                 disabled
-                className="transform opacity-80 cursor-not-allowed transition-all w-8/12 md:w-full lg:w-64 bg-button flex items-center justify-center px-2 py-3 lg:p-4 text-base lg:text-lg text-primary font-bold rounded-md border-none outline-none focus:outline-none"
+                className="transform opacity-80 cursor-not-allowed transition-all w-8/12 md:w-full lg:w-64 bg-button flex items-center justify-center px-2 py-3 lg:p-4 lg:p text-base lg:text-lg text-primary font-bold rounded-md border-none outline-none focus:outline-none"
               >
-                Abrir no navegador <FiGlobe size="25" className="ml-2 lg:ml-3" />
+                ABRIR NO NAVEGADOR <FiGlobe size="25" className="ml-2 lg:ml-3" />
               </button>
             </motion.div>
 
@@ -104,9 +99,9 @@ export default function Home () {
               <span className="text-text-dark font-bold mr-4 md:mr-0">Em breve</span>
               <button
                 disabled
-                className="transform opacity-80 cursor-not-allowed transition-all w-8/12 md:w-full lg:w-64 bg-button flex items-center justify-center px-2 py-3 lg:p-4 text-base lg:text-lg text-primary font-bold rounded-md border-none outline-none focus:outline-none"
+                className="transform opacity-80 cursor-not-allowed transition-all w-8/12 md:w-full lg:w-64 bg-button flex items-center justify-center px-2 py-3 lg:p-4 lg:p text-base lg:text-lg text-primary font-bold rounded-md border-none outline-none focus:outline-none"
               >
-                Download <FiDownload size="24" className="ml-2 lg:ml-3" />
+                DOWNLOAD <FiDownload size="24" className="ml-2 lg:ml-3" />
               </button>
             </motion.div>
 
@@ -117,9 +112,9 @@ export default function Home () {
               <span className="text-text-dark font-bold mr-4 md:mr-0">Em breve</span>
               <button
                 disabled
-                className="transform opacity-80 cursor-not-allowed transition-all w-8/12 md:w-full lg:w-64 bg-button flex items-center justify-center px-2 py-3 lg:p-4 text-base lg:text-lg text-primary font-bold rounded-md border-none outline-none focus:outline-none"
+                className="transform opacity-80 cursor-not-allowed transition-all w-8/12 md:w-full lg:w-64 bg-button flex items-center justify-center px-2 py-3 lg:p-4 lg:p text-base lg:text-lg text-primary font-bold rounded-md border-none outline-none focus:outline-none"
               >
-                Mobile <FiSmartphone size="24" className="ml-2 lg:ml-3" />
+                MOBILE <FiSmartphone size="24" className="ml-2 lg:ml-3" />
               </button>
             </motion.div>
           </motion.div>
@@ -134,7 +129,7 @@ export default function Home () {
 
           <div className="pb-4 md:pb-8 flex items-center justify-center text-text-dark relative">
             <a href="#about" className="cursor-pointer absolute">
-              <FiChevronDown size="24" className="hover:scale-125 transform transition-all" />
+              <FiChevronDown size="30" className="hover:scale-125 transform transition-all" />
             </a>
           </div>
 
@@ -142,24 +137,73 @@ export default function Home () {
 
       </motion.header>
 
-      <main className="min-h-screen bg-background" >
+      <main className="bg-background" >
         <div className="container m-auto">
-          <section id="about">
-            <h3></h3>
-            <p></p>
+          <section className="pt-32" id="about">
+            <h3 className="text-text text-5xl font-bold mb-7">
+              Buscando explicações?<br/>
+              Sério?
+            </h3>
+            <p className="text-2xl">
+              Quero dizer, você usa outro aplicativo igualzinho todo dia.{!isSmallScreen && <br/>}
+              O que quer saber? Se também temos mensagens engraçadas quando alguém entra no servidor?{!isSmallScreen && <br/>}
+              Crie uma conta e dexcubra você mesmo.{!isSmallScreen && <br/>}
+              Mas tome cuidado com os trolls... Por aqui tá infestado deles...
+            </p>
+            <div>
+              <button className="mt-10 py-4 w-52 bg-button border-border border-solid border text-primary rounded-md flex items-center justify-center font-bold text-lg">
+                CRIAR CONTA <FiLogIn className="ml-2" size="24" />
+              </button>
+            </div>
           </section>
 
-          <section id="dev">
-            <h3></h3>
-            <p></p>
+          <section className="pt-32" id="dev">
+            <h3 className="text-text text-5xl font-bold mb-7 text-right">
+              Como funciona?<br/>
+              Finalmente uma pergunta decente
+            </h3>
+            <p className="text-2xl text-right">
+              Uma API em .NET 5 cuida de autenticação e dos servidores, com um banco Postgres por trás.{!isSmallScreen && <br/>}
+              Um servidor Elixir cuida das mensagens, vídeos e áudios, usando Redis como banco de baixa latência{!isSmallScreen && <br/>}
+              e Postgres para armazenamento geral. Você vê tudo em um PWA feito com React e TypeScript.{!isSmallScreen && <br/>}
+              Aonde isso tudo tá rodando? Segredo, mas provavelmente em um contâiner Docker.{!isSmallScreen && <br/>}
+              Não entendeu? Dá uma olhada:
+            </p>
+            <div className="flex justify-end">
+              <button className="mt-10 py-4 w-52 bg-button border-border border-solid border text-primary rounded-md flex items-center justify-center font-bold text-lg">
+                CÓDIGO FONTE <FiGithub className="ml-2" size="24" />
+              </button>
+            </div>
           </section>
 
-          <section id="donate">
-            <h3></h3>
-            <p></p>
+          <section className="py-32" id="donate">
+            <h3 className="text-text text-5xl font-bold mb-7">
+              Me dá um reaaaaal?<br/>
+              Sério, me ajuda ai :D
+            </h3>
+            <p className="text-2xl">
+            No momento, para manter esse projeto funcionando, tenho um gasto mensal de aproximadamente $00.00 dólares{!isSmallScreen && <br/>}
+            por mês. Para um mero estagiário, é f#d@ manter isso. Se você gostou da iniciativa e pode me ajudar,{!isSmallScreen && <br/>}
+            por favor, considere uma doação.{!isSmallScreen && <br/>}
+            Ah, e você ganha uma recompensa na plataforma por isso.
+            </p>
+            <div className="flex gap-10">
+              <button className="mt-10 py-4 w-52 bg-button border-border border-solid border text-primary rounded-md flex items-center justify-center font-bold text-lg">
+                PIX <AiOutlineQrcode className="ml-2" size="24" />
+              </button>
+              <button className="mt-10 py-4 w-52 bg-button border-border border-solid border text-primary rounded-md flex items-center justify-center font-bold text-lg">
+                PICPAY <FiCreditCard className="ml-2" size="24" />
+              </button>
+            </div>
           </section>
         </div>
       </main>
+
+      <footer className="bg-primary">
+        <div className="flex items-center justify-center py-6">
+          <p className="text-text-dark text-lg">Agree 2021 - Nenhum direito reservado :(</p>
+        </div>
+      </footer>
     </motion.div>
   )
 }
