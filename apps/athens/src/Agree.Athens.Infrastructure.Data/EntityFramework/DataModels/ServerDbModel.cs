f4d4a905e55222db49525fd9af904e4ed1a343de
vassourita <vlviniciusguaruja7@gmail.com>
2021-03-22 21:40:43 -0300

    Added server description to db w/ ef migrations

--- apps/athens/src/Agree.Athens.Infrastructure.Data/EntityFramework/DataModels/ServerDbModel.cs
+++ apps/athens/src/Agree.Athens.Infrastructure.Data/EntityFramework/DataModels/ServerDbModel.cs
@@ -20,6 +20,9 @@ namespace Agree.Athens.Infrastructure.Data.EntityFramework.DataModels
         [MaxLength(30)]
         public string Name { get; set; }
 
+        [MaxLength(300)]
+        public string Description { get; set; }
+
         public ICollection<CategoryDbModel> Categories { get; set; }
 
         public ICollection<UserDbModel> Users { get; set; }
