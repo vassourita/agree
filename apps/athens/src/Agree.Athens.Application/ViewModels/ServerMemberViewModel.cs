7aa32ff504650f614cb60a45aae45d21e522b0ca
vassourita <vlviniciusguaruja7@gmail.com>
2021-03-27 22:25:41 -0300

    Updated member view model

--- apps/athens/src/Agree.Athens.Application/ViewModels/ServerMemberViewModel.cs
+++ apps/athens/src/Agree.Athens.Application/ViewModels/ServerMemberViewModel.cs
@@ -3,21 +3,9 @@ using System;
 
 namespace Agree.Athens.Application.ViewModels
 {
-    public class ServerMemberViewModel
+    public class ServerMemberViewModel : AccountViewModel
     {
         public record Role(Guid id);
-
-        public Guid Id { get; set; }
-
-        public string UserName { get; set; }
-
-        public string Email { get; set; }
-
-        public string Tag { get; set; }
-
-        public string AvatarUrl { get; set; }
-
-        public string UserNameWithTag { get => $"{UserName}#{Tag}"; }
 
         public IEnumerable<ServerMemberViewModel.Role> Roles { get; set; }
     }
