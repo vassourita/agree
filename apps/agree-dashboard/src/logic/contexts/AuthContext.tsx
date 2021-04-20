34cd39ad3c3b3c38e2aeba9aea3a49f960bac3d0
vassourita <vlviniciusguaruja7@gmail.com>
2021-04-20 13:31:46 -0300

    Created refresh method to refresh jwts

--- apps/agree-dashboard/src/logic/contexts/AuthContext.tsx
+++ apps/agree-dashboard/src/logic/contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
 import { useToast } from '@chakra-ui/toast'
 import { createContext, ReactNode, useEffect, useState } from 'react'
 import { useHistory } from 'react-router'
+import { useI18n } from '../../presentation/hooks/useI18n'
 import { Account } from '../models/Account'
 import { ICache } from '../services/ICache'
-import { HttpStatusCode, IHttpClient } from '../services/IHttpClient'
+import { HttpResponse, HttpStatusCode, IHttpClient } from '../services/IHttpClient'
 import { ILogger } from '../services/ILogger'
 
 export type AuthContextProps = {
@@ -11,9 +12,10 @@ export type AuthContextProps = {
   account: Account | null
   accessToken: string | null
   refreshToken: string | null
-  login(email: string, password: string): void
+  refresh(): Promise<void>
+  login(email: string, password: string): Promise<void>
   logout(): void
-  register(userName: string, email: string, password: string): void
+  register(userName: string, email: string, password: string): Promise<void>
 }
 
 export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
@@ -33,8 +35,69 @@ export function AuthProvider ({ httpClient, cache, children, logger }: AuthProvi
   const [accessToken, setAccessToken] = useState<string | null>(cache.get<string>(ACCESS_TOKEN_KEY)?.replace('"', '') || null)
   const [refreshToken, setRefreshToken] = useState<string | null>(cache.get<string>(REFRESH_TOKEN_KEY)?.replace('"', '') || null)
 
+  const { t } = useI18n()
+
   const toast = useToast()
   const history = useHistory()
+
+  function logout () {
+    setRefreshToken(null)
+    setAccessToken(null)
+    setAccount(null)
+    history.push('/login')
+  }
+
+  async function refresh (): Promise<void> {
+    const response = await httpClient.request({
+      method: 'post',
+      body: {
+        refreshToken,
+        grantType: 'refresh_token'
+      },
+      url: `${process.env.REACT_APP_API_URL}/accounts/login`
+    })
+
+    if (response.statusCode === HttpStatusCode.OK) {
+      setRefreshToken(response.body.refreshToken)
+      setAccessToken(response.body.accessToken)
+      logger.info(response.body)
+    } else {
+      setRefreshToken(null)
+      setAccessToken(null)
+      logger.info(response.body)
+      toast({
+        title: response.body.message,
+        isClosable: true,
+        status: 'error'
+      })
+    }
+  }
+
+  async function handleUnauthorized (response: HttpResponse) : Promise<boolean> {
+    if (response.statusCode !== HttpStatusCode.UNAUTHORIZED) return true
+
+    if (response.headers['WWW-Authenticate']) {
+      await refresh()
+      return true
+    } else if (response.body?.message === 'Refresh token is expired') {
+      toast({
+        title: t`Your session expired`,
+        description: t`Please login again`,
+        isClosable: true,
+        status: 'info'
+      })
+      logout()
+    } else if (response.body?.message === 'Refresh token is invalid') {
+      toast({
+        title: t`An unexpected error occurred`,
+        description: t`Please login again`,
+        isClosable: true,
+        status: 'info'
+      })
+      logout()
+    }
+    return false
+  }
 
   async function me (): Promise<Account | null> {
     const response = await httpClient.request({
@@ -45,9 +108,10 @@ export function AuthProvider ({ httpClient, cache, children, logger }: AuthProvi
       url: `${process.env.REACT_APP_API_URL}/accounts/@me`
     })
 
-    if (response.statusCode !== HttpStatusCode.OK) {
-      setAccount(null)
-      return null
+    logger.info(response)
+
+    if (response.statusCode !== HttpStatusCode.OK && (await handleUnauthorized(response))) {
+      // return (await me())
     }
     setAccount(response.body.user)
     return response.body.user
@@ -133,15 +197,8 @@ export function AuthProvider ({ httpClient, cache, children, logger }: AuthProvi
     }
   }
 
-  function logout () {
-    setRefreshToken(null)
-    setAccessToken(null)
-    setAccount(null)
-    history.push('/login')
-  }
-
   return (
-    <AuthContext.Provider value={{ account, isAuthenticated: !!account, login, logout, accessToken, refreshToken, register }}>
+    <AuthContext.Provider value={{ account, isAuthenticated: !!account, login, logout, accessToken, refreshToken, register, refresh }}>
       {children}
     </AuthContext.Provider>
   )
