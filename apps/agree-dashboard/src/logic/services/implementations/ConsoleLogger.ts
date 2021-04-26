d5959d168f1d2adf054c145e3e62a4e7c6125941
vassourita <vlviniciusguaruja7@gmail.com>
2021-04-26 00:54:12 -0300

    Removed production logging

--- apps/agree-dashboard/src/logic/services/implementations/ConsoleLogger.ts
+++ apps/agree-dashboard/src/logic/services/implementations/ConsoleLogger.ts
@@ -2,22 +2,18 @@ import { ILogger, LoggingLevel } from '../ILogger'
 
 export class ConsoleLogger implements ILogger {
   public log (level: LoggingLevel, message: unknown): void {
-    console[level](message)
+    if (process.env.NODE_ENV !== 'production') console[level](message)
   }
 
   public info (message: unknown): void {
-    console.log(message)
+    if (process.env.NODE_ENV !== 'production') console.log(message)
   }
 
   public warn (message: unknown): void {
-    console.warn(message)
+    if (process.env.NODE_ENV !== 'production') console.warn(message)
   }
 
   public error (message: unknown): void {
-    console.error(message)
-  }
-
-  private format (level: string, message: unknown) {
-    return `[${level.toUpperCase()} - ${new Date()}] ${message}`
+    if (process.env.NODE_ENV !== 'production') console.error(message)
   }
 }
