ae5b1a1cafdc25ce2749360d7d27eb68e596a6ef
vassourita <vlviniciusguaruja7@gmail.com>
2021-05-14 22:15:27 -0300

    Added server bar tooltips

--- apps/concord/src/presentation/components/avatar/ServerAvatar.tsx
+++ apps/concord/src/presentation/components/avatar/ServerAvatar.tsx
@@ -1,6 +1,7 @@
 import { Image } from '@chakra-ui/image'
 import { Box, Flex, ListItem, Link } from '@chakra-ui/layout'
 import { ChakraProps } from '@chakra-ui/system'
+import { Tooltip } from '@chakra-ui/tooltip'
 import { ReactNode } from 'react'
 import { NavLink as RouterNavLink } from 'react-router-dom'
 
@@ -9,18 +10,21 @@ import './ServerAvatar.scss'
 type ServerAvatarProps = {
   to: string
   icon: ReactNode
+  tooltip?: string
   variant: 'dashed' | 'fill' | 'solid'
 } & ChakraProps
 
-export function ServerAvatar ({ to, icon, variant, ...rest }: ServerAvatarProps): JSX.Element {
+export function ServerAvatar ({ to, icon, variant, tooltip, ...rest }: ServerAvatarProps): JSX.Element {
   if (typeof icon === 'string') {
     if (variant === 'fill') {
       return (
         <Box className="ServerAvatarContainer" {...rest}>
           <Link as={RouterNavLink} to={to} activeClassName="active">
-            <Flex bg="white" w="50px" h="50px" rounded="lg" align="center" justify="center">
-              <Image objectFit="cover" objectPosition="center" src={icon} w="50px" h="50px" rounded="lg" />
-            </Flex>
+            <Tooltip label={tooltip || to} fontSize="md" placement="right">
+              <Flex bg="white" w="50px" h="50px" rounded="lg" align="center" justify="center">
+                <Image objectFit="cover" objectPosition="center" src={icon} w="50px" h="50px" rounded="lg" />
+              </Flex>
+            </Tooltip>
           </Link>
         </Box>
       )
@@ -29,9 +33,11 @@ export function ServerAvatar ({ to, icon, variant, ...rest }: ServerAvatarProps)
     return (
       <ListItem className="ServerAvatarContainer" marginY="0.5rem" {...rest}>
         <Link as={RouterNavLink} to={to} activeClassName="active">
-          <Flex bg="white" w="50px" h="50px" rounded="lg">
-            <Image objectFit="cover" objectPosition="center" src={icon} w="50px" h="50px" rounded="lg" />
-          </Flex>
+          <Tooltip label={tooltip || to} fontSize="md" placement="right">
+            <Flex bg="white" w="50px" h="50px" rounded="lg">
+              <Image objectFit="cover" objectPosition="center" src={icon} w="50px" h="50px" rounded="lg" />
+            </Flex>
+          </Tooltip>
         </Link>
       </ListItem>
     )
@@ -40,9 +46,11 @@ export function ServerAvatar ({ to, icon, variant, ...rest }: ServerAvatarProps)
   return (
     <Box className="ServerAvatarContainer" {...rest}>
       <Link as={RouterNavLink} to={to} activeClassName="active">
-        <Flex border={`2px ${variant} white`} bg="none" w="50px" h="50px" rounded="lg" align="center" justify="center">
-          {icon}
-        </Flex>
+        <Tooltip label={tooltip || to} fontSize="md" placement="right">
+          <Flex border={`2px ${variant} white`} bg="none" w="50px" h="50px" rounded="lg" align="center" justify="center">
+            {icon}
+          </Flex>
+        </Tooltip>
       </Link>
     </Box>
   )
--- apps/concord/src/presentation/components/nav/ServerBar.tsx
+++ apps/concord/src/presentation/components/nav/ServerBar.tsx
@@ -2,15 +2,18 @@ import { Box, Flex, List } from '@chakra-ui/layout'
 import { FiCompass, FiPlus, FiSettings } from 'react-icons/fi'
 
 import IconImg from '../../assets/iconw.svg'
+import { useI18n } from '../../hooks/useI18n'
 import { ServerAvatar } from '../avatar/ServerAvatar'
 
 export function ServerBar (): JSX.Element {
+  const { t } = useI18n()
+
   return (
     <Flex flexDirection="column" align="center" justify="space-between" position="relative" h="100vh" minW="100px" bg="brand.600" roundedRight="md">
 
       <Flex flexDirection="column" align="center" justify="center" minW="100px" marginTop="1.75rem" marginBottom="1.25rem">
 
-        <ServerAvatar to="/home" variant="fill" icon={IconImg} marginBottom="1.75rem" />
+        <ServerAvatar tooltip="Home" to="/home" variant="fill" icon={IconImg} marginBottom="1.75rem" />
 
         <Box border="1px solid white" width="75px" />
 
@@ -22,9 +25,9 @@ export function ServerBar (): JSX.Element {
           <ServerAvatar key={i} variant="solid" icon="https://source.unsplash.com/random" to={`/s/${i}`}/>
         ))}
 
-        <ServerAvatar to="/s/new" variant="dashed" icon={<FiPlus color="white" size={24} />} marginY="0.5rem" />
+        <ServerAvatar tooltip={t`Create server`} to="/s/new" variant="dashed" icon={<FiPlus color="white" size={24} />} marginY="0.5rem" />
 
-        <ServerAvatar to="/s/search" variant="dashed" icon={<FiCompass color="white" size={24} />} marginY="0.5rem" />
+        <ServerAvatar tooltip={t`Search servers`} to="/s/search" variant="dashed" icon={<FiCompass color="white" size={24} />} marginY="0.5rem" />
 
       </List>
 
@@ -32,7 +35,7 @@ export function ServerBar (): JSX.Element {
 
         <Box border="1px solid white" width="75px" />
 
-        <ServerAvatar to="/settings" variant="solid" icon={<FiSettings color="white" size={24} />} marginTop="1.75rem" />
+        <ServerAvatar tooltip={t`Settings`} to="/settings" variant="solid" icon={<FiSettings color="white" size={24} />} marginTop="1.75rem" />
 
       </Flex>
 
