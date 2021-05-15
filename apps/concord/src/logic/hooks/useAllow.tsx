515d8915c1be5b74596c5f505ff915d0f0c10c21
vassourita <vlviniciusguaruja7@gmail.com>
2021-05-15 00:10:54 -0300

    Fixed private routes rendering

--- apps/concord/src/logic/hooks/useAllow.tsx
+++ apps/concord/src/logic/hooks/useAllow.tsx
@@ -1,40 +1,35 @@
-import { FunctionComponent, useContext } from 'react'
+import { useContext } from 'react'
 import { Redirect, Route, RouteProps } from 'react-router'
 import { AllowContext, AllowContextProps } from '../contexts/AllowContext'
 
-type AuthHook = {
-  AuthenticatedRoute: FunctionComponent<RouteProps>
-  UnauthenticatedRoute: FunctionComponent<RouteProps>
-} & AllowContextProps
-
-export function useAllow (): AuthHook {
+export function useAllow (): AllowContextProps {
   const ctx = useContext(AllowContext)
 
-  function AuthenticatedRoute ({ component, ...rest }: RouteProps): JSX.Element {
-    const Component = component as any
-    return (
-      <Route {...rest} render={props => (
-        ctx.isAuthenticated
-          ? <Component {...props} />
-          : <Redirect to="/login" />
-      )} />
-    )
-  }
-
-  function UnauthenticatedRoute ({ component, ...rest }: RouteProps): JSX.Element {
-    const Component = component as any
-    return (
-      <Route {...rest} render={props => (
-        !ctx.isAuthenticated
-          ? <Component {...props} />
-          : <Redirect to="/home" />
-      )} />
-    )
-  }
-
   return {
-    ...ctx,
-    AuthenticatedRoute,
-    UnauthenticatedRoute
+    ...ctx
   }
+}
+
+export function AuthenticatedRoute ({ component, ...rest }: RouteProps): JSX.Element {
+  const ctx = useContext(AllowContext)
+  const Component = component as any
+  return (
+    <Route {...rest} render={props => (
+      ctx.isAuthenticated
+        ? <Component {...props} />
+        : <Redirect to="/login" />
+    )} />
+  )
+}
+
+export function UnauthenticatedRoute ({ component, ...rest }: RouteProps): JSX.Element {
+  const ctx = useContext(AllowContext)
+  const Component = component as any
+  return (
+    <Route {...rest} render={props => (
+      !ctx.isAuthenticated
+        ? <Component {...props} />
+        : <Redirect to="/home" />
+    )} />
+  )
 }
--- apps/concord/src/presentation/Routes.tsx
+++ apps/concord/src/presentation/Routes.tsx
@@ -1,40 +1,27 @@
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { useAllow } from '../logic/hooks/useAllow'
+import { AuthenticatedRoute, UnauthenticatedRoute } from '../logic/hooks/useAllow'
 import { DashboardLayout } from './layouts/DashboardLayout'
 import { AuthPage } from './pages/account/AuthPage'
 import { HomePage } from './pages/dashboard/HomePage'
 import { NotFoundPage } from './pages/error/NotFoundPage'
 
 export function Routes (): JSX.Element {
-  const { AuthenticatedRoute, UnauthenticatedRoute } = useAllow()
-
   return (
     <Switch>
-      <DashboardLayout>
-      <AuthenticatedRoute exact path="/" component={() => (
-        <Redirect to="/home"/>
+      <AuthenticatedRoute exact path={['/', '/home', '/settings', '/s/new', '/s/search', '/s/:id']} component={() => (
+        <DashboardLayout>
+          <Route render={({ location }) => (
+            <Switch location={location}>
+              <AuthenticatedRoute exact path="/" component={() => <Redirect to="/home" />}/>
+              <AuthenticatedRoute exact path="/home" component={() => <HomePage />}/>
+              <AuthenticatedRoute exact path="/settings" component={() => <HomePage />}/>
+              <AuthenticatedRoute exact path="/s/search" component={() => <HomePage />}/>
+              <AuthenticatedRoute exact path="/s/new" component={() => <HomePage />}/>
+              <AuthenticatedRoute exact path="/s/:id" component={() => <HomePage />}/>
+            </Switch>
+          )}/>
+        </DashboardLayout>
       )}/>
-
-      <AuthenticatedRoute exact path="/home" component={() => (
-        <HomePage />
-      )}/>
-
-      <AuthenticatedRoute exact path="/settings" component={() => (
-        <HomePage />
-      )}/>
-
-      <AuthenticatedRoute exact path="/s/new" component={() => (
-        <HomePage />
-      )}/>
-
-      <AuthenticatedRoute exact path="/s/search" component={() => (
-        <HomePage />
-      )}/>
-
-      <AuthenticatedRoute exact path="/s/:id" component={() => (
-        <HomePage />
-      )}/>
-      </DashboardLayout>
 
       <UnauthenticatedRoute exact path={['/login', '/register']} component={() => (
         <AuthPage />
