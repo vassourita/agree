c3475dbe821d1a4559f7b71ef7aec9029030d4ce
vassourita <vlviniciusguaruja7@gmail.com>
2021-05-17 19:59:29 -0300

    Added basic settings page styling

--- apps/concord/src/presentation/pages/account/SettingsPage.tsx
+++ apps/concord/src/presentation/pages/account/SettingsPage.tsx
@@ -1,9 +1,12 @@
-import { Flex, Heading } from '@chakra-ui/layout'
+import { Flex, Heading, Text } from '@chakra-ui/layout'
 
 export function SettingsPage (): JSX.Element {
   return (
-    <Flex flexDirection="column">
-      <Heading>/settings</Heading>
+    <Flex flexDir="column" w="100%">
+      <Flex fontWeight="bold" flexDir="column" p="3rem">
+        <Text>Ah, então você é desses que gosta de personalizar tudo né?</Text>
+        <Heading fontSize="5xl">Configurações</Heading>
+      </Flex>
     </Flex>
   )
 }
