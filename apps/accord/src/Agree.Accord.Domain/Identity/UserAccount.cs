73172773c611a18a9a43db7a0d21854a50eda709
Vinicius Vassão <vlviniciusguaruja7@gmail.com>
2021-07-16 14:46:37 -0300

    Finished user docsw

--- apps/accord/src/Agree.Accord.Domain/Identity/UserAccount.cs
+++ apps/accord/src/Agree.Accord.Domain/Identity/UserAccount.cs
@@ -20,20 +20,48 @@ namespace Agree.Accord.Domain.Identity
             Tag = tag;
         }
 
+        /// <summary>
+        /// Updates the user display name.
+        /// </summary>
+        /// <param name="newUserName">The new user display name.</param>
         public void UpdateUserName(string newUserName)
         {
             UserName = newUserName;
             Update();
         }
 
+        /// <summary>
+        /// Updates the user email.
+        /// </summary>
+        /// <param name="newUserName">The new user email.</param>
         public void UpdateEmail(string newEmail)
         {
             Email = newEmail;
             Update();
         }
 
+        /// <summary>
+        /// Gets the user display name.
+        /// </summary>
+        /// <value>The user display name.</value>
         public string UserName { get; private set; }
+
+        /// <summary>
+        /// Gets the user nametag. The nametag is a unique identifier formed by the user display name and the discriminator tag.
+        /// </summary>
+        /// <value>The user nametag.</value>
+        public string NameTag { get => $"{UserName}#{Tag.ToString()}"; }
+
+        /// <summary>
+        /// Gets the user email address.
+        /// </summary>
+        /// <value>The user email.</value>
         public string Email { get; private set; }
+
+        /// <summary>
+        /// Gets the user email discriminator tag.
+        /// </summary>
+        /// <value>The user discriminator tag.</value>
         public DiscriminatorTag Tag { get; private set; }
     }
 }
