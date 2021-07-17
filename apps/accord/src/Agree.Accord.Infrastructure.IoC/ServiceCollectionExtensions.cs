54264c53458eb08855b41eb50423243729bc7016
Vinicius Vassão <vlviniciusguaruja7@gmail.com>
2021-07-17 05:11:05 -0300

    Setup dbctx on ioc

--- apps/accord/src/Agree.Accord.Infrastructure.IoC/ServiceCollectionExtensions.cs
+++ apps/accord/src/Agree.Accord.Infrastructure.IoC/ServiceCollectionExtensions.cs
@@ -1,12 +1,18 @@
 using Agree.Accord.Domain.Identity.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Agree.Accord.Infrastructure.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace Agree.Accord.Infrastructure.IoC
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddAccordInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddAccordInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
             services.AddScoped<AccountService>();
 
             return services;
--- apps/accord/src/Agree.Accord.Presentation/Startup.cs
+++ apps/accord/src/Agree.Accord.Presentation/Startup.cs
@@ -27,7 +27,7 @@ namespace Agree.Accord.Presentation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAccordInfrastructure();
+            services.AddAccordInfrastructure(Configuration);
             services.AddAccordAuthentication();
 
             services.AddControllers();
