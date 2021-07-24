37c75f6f3312a0a28830460dca7d00f3b5526c80
Vinicius Vassão <vlviniciusguaruja7@gmail.com>
2021-07-24 15:26:53 -0300

    Updated startup chained call

--- apps/accord/src/Agree.Accord.Presentation/Startup.cs
+++ apps/accord/src/Agree.Accord.Presentation/Startup.cs
@@ -28,8 +28,8 @@ namespace Agree.Accord.Presentation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAccordInfrastructure(Configuration);
-            services.AddAccordAuthentication(Configuration);
+            services.AddAccordInfrastructure(Configuration)
+                .AddAccordAuthentication(Configuration);
 
             services.AddControllers().ConfigureApiBehaviorOptions(options =>
             {
